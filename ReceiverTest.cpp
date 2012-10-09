3736d6b56ec26a00d0b94bb22d6b82f47aa9df6b
Nik Karpinsky <karpinsn@iastate.edu>
2012-10-09 16:07:35 -0500

    Made the video loop

--- ReceiverTest.cpp
+++ ReceiverTest.cpp
@@ -5,16 +5,29 @@ int main(int argc, char** argv)
 	antenna::BaseStation testReceiver;
 	testReceiver.start(7681);
 
-	IplImage* img = cvLoadImage( "N:/Data/0000.png" );
+	CvCapture* mov = cvCaptureFromAVI( "D:/Data/Jane.avi" );
 
 	int p[] = {CV_IMWRITE_JPEG_QUALITY, 100, 0};
-	CvMat* buf = cvEncodeImage(".jpg", img, p);
-	//CvMat* buf = cvEncodeImage(".png", img, 0);
+
+	int frameNumber = 0;
 
 	while(testReceiver.yieldTime() >= 0)
 	{
-	  testReceiver.broadcastData(buf->data.ptr, buf->width);
-	  usleep(1000/30);
+		IplImage* img = cvQueryFrame(mov);
+		frameNumber++;
+		if(!img || frameNumber > 600)
+		{
+			frameNumber = 0;
+			cvReleaseCapture(&mov);
+			mov = cvCaptureFromAVI( "D:/Data/Jane.avi" );
+			continue;
+		}
+
+		CvMat* buf = cvEncodeImage(".jpg", img, p);
+		testReceiver.broadcastData(buf->data.ptr, buf->width);
+		cvReleaseMat(&buf);
+
+		usleep(1000/30);
 	}
 
     return 0;
